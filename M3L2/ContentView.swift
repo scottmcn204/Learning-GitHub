c6689a317113371975008a95ba0887979d13ad15
Scott <scottamcnally@gmail.com>
2022-08-12 17:22:28 +0100

    Initial Commit

---
+++ M3L2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M3L2
+//
+//  Created by Scott McNally on 12/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3L2/M3L2App.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2App.swift
+//  M3L2
+//
+//  Created by Scott McNally on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct M3L2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
